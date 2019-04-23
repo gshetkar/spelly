b6e9da2aea7db8f9c3de00bd360d7919db00667e
Anurag-Patil <cs17btech11004@iith.ac.in>
2019-04-24 03:19:52 +0530

    Edit-Distance function to evaluate minimum possible distance between 2 string

---
+++ Edit_Distance.hs
@@ -0,0 +1,26 @@
+module Edit_Distance
+(dist
+) where
+	
+import qualified Data.Vector as V
+
+dist :: Eq a => [a] -> [a] -> Int
+dist a b 
+    = last (if lab == 0 then mainDiag
+	    else if lab > 0 then lowers !! (lab - 1)
+		 else   uppers !! (-1 - lab))
+    where mainDiag = oneDiag a b (head uppers) (-1 : head lowers)
+	  uppers = eachDiag a b (mainDiag : uppers) 
+	  lowers = eachDiag b a (mainDiag : lowers) 
+	  eachDiag a [] diags = []
+	  eachDiag a (bch:bs) (lastDiag:diags) = oneDiag a bs nextDiag lastDiag : eachDiag a bs diags
+	      where nextDiag = head (tail diags)
+	  oneDiag a b diagAbove diagBelow = thisdiag
+	      where doDiag [] b nw n w = []
+		    doDiag a [] nw n w = []
+		    doDiag (ach:as) (bch:bs) nw n w = me : (doDiag as bs me (tail n) (tail w))
+			where me = if ach == bch then nw else 1 + min3 (head w) nw (head n)
+		    firstelt = 1 + head diagBelow
+		    thisdiag = firstelt : doDiag a b firstelt diagAbove (tail diagBelow)
+	  lab = length a - length b
+          min3 x y z = if x < y then x else min y z
