fe4b0bbbe880e5f92924e85e2258e12e1187bee4
gshetkar <cs17btech11016@iith.ac.in>
2019-04-25 20:37:29 +0530

    relocating files to src

--- spelly/app/Main.hs
+++ spelly/app/Main.hs
@@ -1,6 +1,12 @@
-module Main where
+import System.IO
+import System.Environment
+import Data.Char
+import String_Operation
 
-import Lib
 
-main :: IO ()
-main = someFunc
+main = do
+	handle_text_file <- openFile "Editor.txt" ReadMode
+	handle_dictionary <- openFile "Frequencies.txt" ReadMode
+	editor_text <- hGetContents handle_text_file
+	default_dictionary <- hGetContents handle_dictionary
+	putStrLn (myoutput (map toLower editor_text) (map toLower default_dictionary))
